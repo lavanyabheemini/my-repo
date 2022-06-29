0d6abda956e26b29781f490414cc1ebb1269c580
lavanyabheemini <lavanyabheemini99@gmail.com>
2022-06-29 10:55:47 +0530

    Initial commit

---
+++ src/main/java/com/epam/demo/demoprojectsetup/DemoGradleProjectSetupApplication.java
@@ -0,0 +1,14 @@
+package com.epam.demo.demoprojectsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradleProjectSetupApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java you are near");
+		SpringApplication.run(DemoGradleProjectSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/demo/demoprojectsetup/DemoGradleProjectSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.demo.demoprojectsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradleProjectSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
